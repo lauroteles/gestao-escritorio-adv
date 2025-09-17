fd4714bad4fb938bbedbbbd29bc86d1bd7100775
lauroteles <laurotfl@gmail.com>
2025-09-17 14:11:31 -0300

    feat - DTO

--- src/main/java/com/gestaoescritorio/gestao/dto/processoDTO.java
+++ src/main/java/com/gestaoescritorio/gestao/dto/processoDTO.java
@@ -2,9 +2,7 @@ package com.gestaoescritorio.gestao.dto;
 
 import com.gestaoescritorio.gestao.entity.AndamentosEntity;
 
-import java.util.List;
+public record processoDTO(String processoNumero, String nome, String tribunal) {};
 
-public record processoDTO(String processoNumero, String nome, String tribunal) {
-}
 
 
