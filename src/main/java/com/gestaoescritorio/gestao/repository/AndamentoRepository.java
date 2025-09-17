71baa05402ed87a14c56e37772e93eb025031915
lauroteles <laurotfl@gmail.com>
2025-09-17 14:11:22 -0300

    feat - repositories

--- src/main/java/com/gestaoescritorio/gestao/repository/AndamentoRepository.java
+++ src/main/java/com/gestaoescritorio/gestao/repository/AndamentoRepository.java
@@ -1,9 +1,15 @@
 package com.gestaoescritorio.gestao.repository;
 
 import com.gestaoescritorio.gestao.entity.AndamentosEntity;
+import org.aspectj.weaver.ast.And;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface AndamentoRepository extends JpaRepository<AndamentosEntity, Long> {
+
+    List<AndamentosEntity> findByProcesso_ProcessoNumero(String processoNumero);
+
 
 }
--- src/main/java/com/gestaoescritorio/gestao/repository/ProcessoRepository.java
+++ src/main/java/com/gestaoescritorio/gestao/repository/ProcessoRepository.java
@@ -1,11 +1,13 @@
 package com.gestaoescritorio.gestao.repository;
 
 import com.gestaoescritorio.gestao.entity.ProcessosEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.expression.spel.ast.OpAnd;
 
 import java.util.Optional;
 
 public interface ProcessoRepository extends JpaRepository<ProcessosEntity, Long> {
     Optional<ProcessosEntity>  findByProcessoNumero(String processoNumero);
+
 }
